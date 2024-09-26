a60c8597c76b3895ad92f81eaaa10352c42dc298
오승준 <skku03062@g.skku.edu>
2024-09-26 15:42:50 +0900

    네이버 지도 api 추가

--- app/(afterLogin)/home/_components/Map.jsx
+++ app/(afterLogin)/home/_components/Map.jsx
@@ -1,9 +1,32 @@
 "use client";
-import React from "react";
+import { useEffect } from "react";
+import Head from "next/head";
 import styles from "./map.module.css";
 
-const Map = () => {
-  return <div className={styles.container}></div>;
+const NaverMap = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_ID}&submodules=geocoder`;
+    script.async = true;
+    script.onload = () => {
+      const mapDiv = document.getElementById("map");
+      const map = new naver.maps.Map(mapDiv);
+    };
+    document.head.appendChild(script);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <script
+          type="text/javascript"
+          src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_NAVER_ID}&submodules=geocoder`}
+          async
+        ></script>
+      </Head>
+      <div id="map" className={styles.map}></div>
+    </div>
+  );
 };
 
-export default Map;
+export default NaverMap;
--- app/(afterLogin)/layout.jsx
+++ app/(afterLogin)/layout.jsx
@@ -1,4 +1,5 @@
 import styles from "./layout.module.css";
+// import { NavermapsProvider } from "react-naver-maps";
 
 const Layout = ({ children }) => {
   return <div className={styles.container}>{children}</div>;
