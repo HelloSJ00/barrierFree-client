cd8da1f97afe490b7d0ff0027607fa5bf6ada262
오승준 <skku03062@g.skku.edu>
2024-09-28 14:08:09 +0900

    add recommend page

--- app/(afterLogin)/home/_components/Navbar.jsx
+++ app/(afterLogin)/home/_components/Navbar.jsx
@@ -5,6 +5,10 @@ import LogoutIcon from "@mui/icons-material/Logout";
 import { AppBar, Toolbar, Button, Typography, Box } from "@mui/material";
 import Link from "next/link";
 
+//ICON
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import BookmarksIcon from "@mui/icons-material/Bookmarks";
+
 const Navbar = () => {
   const onClickLogout = () => {};
 
@@ -53,13 +57,37 @@ const Navbar = () => {
               </Button>
             </Link>
             <Link href="/home/bookmarks" passHref>
-              <Button color="inherit" sx={{ fontSize: 20 }}>
+              <Button
+                color="inherit"
+                sx={{
+                  fontSize: 20,
+                  display: "flex", // 아이콘과 텍스트를 가로로 정렬
+                  alignItems: "center", // 세로 가운데 정렬
+                  gap: 1, // 아이콘과 텍스트 사이의 간격 설정
+                }}
+              >
+                <BookmarksIcon sx={{ fontSize: 24 }} />
                 즐겨찾기
               </Button>
             </Link>
+
             <Link href="/user" passHref>
-              <Button color="inherit" sx={{ fontSize: 20 }}>
+              <Button
+                color="inherit"
+                sx={{
+                  fontSize: 20,
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                }}
+              >
+                <AccountCircleIcon sx={{ fontSize: 24 }} />
                 계정
+              </Button>
+            </Link>
+            <Link href="/home/recommended" passHref>
+              <Button color="inherit" sx={{ fontSize: 20 }}>
+                임시 추천
               </Button>
             </Link>
           </Box>
@@ -75,6 +103,9 @@ const Navbar = () => {
             color: "#1b5e20",
             borderColor: "#1b5e20",
             fontSize: 20,
+            display: "flex",
+            alignItems: "center",
+            gap: 1,
             borderRadius: 50,
             boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)", // 그림자 효과 추가
             "&:hover": {
@@ -83,8 +114,8 @@ const Navbar = () => {
           }}
           variant="outlined"
         >
-          로그아웃
           <LogoutIcon sx={12} />
+          로그아웃
         </Button>
       </footer>
     </div>
---
+++ app/(afterLogin)/home/bookmarks/_components/BookmarkCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookmarkCard = () => {
+  return <div></div>;
+};
+
+export default BookmarkCard;
