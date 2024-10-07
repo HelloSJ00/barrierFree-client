804fc25ffdf08e82b02a321989f1aac1dc20cdbb
오승준 <skku03062@g.skku.edu>
2024-10-07 14:34:09 +0900

    시작화면 수정

--- app/(beforeLogin)/_components/Main.jsx
+++ app/(beforeLogin)/_components/Main.jsx
@@ -3,13 +3,20 @@ import React from "react";
 import styles from "./main.module.css";
 import Button from "@mui/material/Button";
 import BasicButton from "./common/BasicButton";
+import KBarrierFreeLogo from "@/app/_components/KBarrierFreeLogo";
 
 const Main = () => {
   return (
     <div className={styles.container}>
       <div className={styles.leftSection}>
-        <h1>눈치보지 말고 편하게 외출하는 서비스</h1>
-        <h2>배리어 - 프리</h2>
+        <p>
+          <strong>
+            <i>눈치보지 말고 편하게 외출하는 서비스</i>
+          </strong>
+        </p>
+        <div>
+          <KBarrierFreeLogo></KBarrierFreeLogo>
+        </div>
       </div>
       <div className={styles.rightSection}>
         <h2>지금 바로 이용하세요</h2>
