378c13e885e1d9b2a15628a6b5aa863be71b2683
오승준 <skku03062@g.skku.edu>
2024-10-29 18:41:44 +0900

    빌드오류 수정

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,28 +1,29 @@
 "use client";
 import { useEffect, useRef } from "react";
-import lottie from "lottie-web"; // 정적 import
+import dynamic from "next/dynamic";
 import styles from "./loading.module.css";
 
 const Loading = () => {
   const containerRef = useRef(null);
-  const animationInstance = useRef(null);
 
   useEffect(() => {
-    if (!animationInstance.current && containerRef.current) {
-      animationInstance.current = lottie.loadAnimation({
+    let animationInstance;
+
+    const loadAnimation = async () => {
+      const lottie = await import("lottie-web");
+      animationInstance = lottie.loadAnimation({
         container: containerRef.current,
         renderer: "svg",
         loop: true,
         autoplay: true,
         path: "/animations/Animation - 1729675646601.json",
       });
-    }
+    };
+
+    loadAnimation();
 
     return () => {
-      if (animationInstance.current) {
-        animationInstance.current.destroy();
-        animationInstance.current = null;
-      }
+      if (animationInstance) animationInstance.destroy();
     };
   }, []);
 
